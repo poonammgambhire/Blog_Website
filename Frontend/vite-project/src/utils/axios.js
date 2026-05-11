38b6272653ac1c4d2040e436fb367a31226988e1
poonam gambhire <poonammgambhire964@gmail.com>
2026-05-11 12:50:12 +0530

    updated backend url

--- Frontend/vite-project/src/utils/axios.js
+++ Frontend/vite-project/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://hrm-backend-a7xi.onrender.com/api',
 })
 
 API.interceptors.request.use((req) => {
